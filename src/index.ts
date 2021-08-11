9d1bb37fcbf7ca9d588c7feaebfd1c468e49c1bd
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2021-08-11 09:48:29 +0900

    debounces & distinctUntiles & pluck & samples & takeUntil & throttles

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,46 @@
-import { from, fromEvent, interval } from "rxjs";
+import { asyncScheduler, from, fromEvent, interval, of } from "rxjs";
 import {
+  debounce,
+  debounceTime,
+  distinctUntilChanged,
+  distinctUntilKeyChanged,
   filter,
+  first,
   map,
   mapTo,
+  pluck,
   reduce,
+  sample,
+  sampleTime,
   scan,
   startWith,
   take,
+  takeUntil,
   takeWhile,
+  tap,
+  throttle,
+  throttleTime,
 } from "rxjs/operators";
 
 // el's
 const progressBar: HTMLElement = document.querySelector(".progress-bar");
+const textInput: HTMLElement = document.getElementById("text-input");
 const countdown: HTMLElement = document.getElementById("countdown");
 const message: HTMLElement = document.getElementById("message");
 
 const scroll$ = fromEvent(document, "scroll");
+const click$ = fromEvent<MouseEvent>(document, "click");
+const input$ = fromEvent(textInput, "keyup");
+const timer$ = interval(1000);
+
 const progress$ = scroll$.pipe(
-  // percent progress
-  map(({ target }: any) => calculateScrollPercent(target.documentElement))
+  throttleTime(30, asyncScheduler, {
+    leading: false,
+    trailing: true,
+  }),
+  // percent-progress
+  map(({ target }: any) => calculateScrollPercent(target.documentElement)),
+  tap(console.log)
 );
 
 progress$.subscribe((percent) => {
@@ -32,40 +54,147 @@ function calculateScrollPercent(el) {
   return (scrollTop / (scrollHeight - clientHeight)) * 100;
 }
 
+// ------------------------------- //
+
 const nums = [1, 2, 3, 4, 5];
 const user = [
   { name: "Brain", loggedIn: false, token: null },
   { name: "Brain", loggedIn: true, token: "abc" },
   { name: "Brain", loggedIn: true, token: "123" },
 ];
+const counter$ = interval(1000);
 
+// Reduce
 const totalReducer = (acc, current) => {
   return acc + current;
 };
+from(nums).pipe(reduce(totalReducer));
+// .subscribe(console.log);
 
+// Scan
 const state$ = from(user).pipe(
   scan((acc, current) => {
     return { ...acc, ...current };
   })
 );
 
+// Map
 const name$ = state$.pipe(map((state) => state.name));
 
-const counter$ = interval(1000);
+// MapTo, Scan, Tap, Filter
+counter$.pipe(
+  mapTo(-1),
+  scan((acc, current) => {
+    return acc + current;
+  }, 10),
+  tap(console.log),
+  filter((v) => v >= 0)
+);
+// .subscribe((v) => {
+//   countdown.innerHTML = `${v}`;
+//   if (!v) {
+//     message.innerHTML = "Liftoff!";
+//   }
+// });
 
-counter$
-  .pipe(
-    mapTo(-1),
-    scan((acc, current) => {
-      return acc + current;
-    }, 10),
-    filter((v) => v >= 0)
-  )
-  .subscribe((v) => {
-    countdown.innerHTML = `${v}`;
-    if (!v) {
-      message.innerHTML = "Liftoff!";
-    }
-  });
+// Take
+from(nums).pipe(take(3));
+// .subscribe({
+//   next: console.log,
+//   complete: () => console.log("Complete!"),
+// });
 
-counter$.subscribe(console.log);
+// First
+click$.pipe(
+  map((event: MouseEvent) => ({
+    x: event.clientX,
+    y: event.clientY,
+  })),
+  // filter, take(1)
+  first(({ y }) => y > 200)
+);
+// .subscribe({
+//   next: console.log,
+//   complete: () => console.log("Complete!"),
+// });
+
+// Take While
+click$.pipe(
+  map((event: MouseEvent) => ({
+    x: event.clientX,
+    y: event.clientY,
+  })),
+  takeWhile(({ y }) => y <= 200),
+  take(2)
+);
+// .subscribe({
+//   next: console.log,
+//   complete: () => console.log("Complete!"),
+// });
+
+counter$.pipe(
+  mapTo(-1),
+  tap(console.log),
+  scan((acc, current) => {
+    console.log(acc);
+    return acc + current;
+  }, 6),
+  takeWhile((v) => v >= 0)
+);
+// .subscribe((v) => {
+//   countdown.innerHTML = `${v}`;
+//   if (!v) {
+//     message.innerHTML = "Liftoff!";
+//   }
+// });
+
+// Take Until
+counter$.pipe(takeUntil(click$));
+//   .subscribe({
+//   next: console.log,
+//   complete: () =>
+//     console.log("%c Complete!", "color: green; font-weight: 700;"),
+// });
+
+// DistinctUntilChanged
+const source$ = from([
+  { name: "Brian" },
+  { name: "Joe" },
+  { name: "Joe" },
+  { name: "Sue" },
+]);
+source$
+  // so booring ...
+  .pipe(distinctUntilChanged((prev, { name }) => name === prev.name));
+// .subscribe(console.log);
+source$
+  // custom compare based on name property
+  .pipe(distinctUntilKeyChanged("name"));
+// .subscribe(console.log);
+
+// Debounce && DebounceTime
+click$.pipe(debounceTime(1000), pluck("target", "value"));
+input$.pipe(
+  debounceTime(1000),
+  pluck("target", "value"),
+  distinctUntilChanged()
+);
+// .subscribe(console.log);
+input$.pipe(
+  debounce(() => interval(1000)),
+  pluck("target", "value"),
+  distinctUntilChanged()
+);
+// .subscribe((v) => console.log("the original ", v));
+
+// ThrottleTime
+click$.pipe(throttleTime(3000));
+// .subscribe(console.log);
+
+// Sample && SampleTime
+click$.pipe(
+  sampleTime(4000),
+  map(({ clientX, clientY }) => ({ clientX, clientY }))
+);
+// .subscribe(console.log);
+timer$.pipe(sample(click$)).subscribe(console.log);

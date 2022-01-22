b77b6586b162e6e9b588d360e300e35f4193ccb8
ippatev <ippatiev_ag@albank.ru>
2022-01-23 02:37:29 +0900

    ajax operator, done!

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { todos$ } from './operators/ajax'
+import { todos$, users$ } from './operators/ajax'
 
 todos$.subscribe(todos => {
     console.log('todos ',todos)
+})
+
+users$.subscribe(req => {
+    console.log('users ', JSON.parse(req.response))
 })
 
 console.log('hello')
--- src/operators/ajax.ts
+++ src/operators/ajax.ts
@@ -2,10 +2,32 @@ import {ajax} from 'rxjs/ajax';
 import {catchError, tap} from "rxjs/operators";
 import {of} from "rxjs";
 
-const todos$ = ajax.getJSON('https://jsonplaceholder.typicode.com/todos')
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+const todos$ = ajax.getJSON(`${BASE_URL}/todos`)
     .pipe(
         tap((data) => {console.log('side effects with tap operator. ',data[0].userId)}),
         catchError((err) => of(err))
     )
 
-export {todos$}
+/**
+ * @return ajax data model
+*/
+const users$ = ajax({
+    responseType: 'GET',
+    url: `${BASE_URL}/users`,
+    headers: {
+        'Content-Type': 'application/json',
+        'custom-header': 'hello',
+    },
+    /**
+     * @description Dude, you can't attach a body to a get request,
+     * are you kidding me?
+     */
+    body: null
+})
+
+export {
+    todos$,
+    users$
+}
